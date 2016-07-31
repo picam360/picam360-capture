f7aa74f7d5dc45559977eb2ca8ab284c0fa68db4
takumadx <hirayama.takuma@ohmydigifab.com>
2016-07-31 18:04:05 +0900

    bugfix

--- shader/vertshader.glsl
+++ shader/vertshader.glsl
@@ -4,7 +4,7 @@ varying vec2 tcoord;
 void main(void)
 {
     vec4 pos = vPosition;
-    tcoord = pos.xy;
     pos.xy = pos.xy*vec2(2,2) + vec2(-1,-1);
     gl_Position = pos;
+    tcoord = pos.xy;
 }
