e8b7464ee3497f49277b711d0328e38becb841db
takumadx <hirayama.takuma@ohmydigifab.com>
2016-10-19 19:33:27 +0900

    snap

--- triangle.c
+++ triangle.c
@@ -677,6 +677,7 @@ int main(int argc, char *argv[]) {
 		if(state->preview) {
 			redraw_scene(state);
 		}
+		glFlush();
 
 		if(state->snap && state->snap_save_path[0] != '\0') {
 			state->snap = false;
@@ -687,15 +688,10 @@ int main(int argc, char *argv[]) {
 			unsigned char *buff = (unsigned char*)malloc(size);
 
 			glFinish();
-			glFlush();
-			eglSwapBuffers(state->display, state->surface);
-			glFinish();
 
 			glBindFramebuffer(GL_FRAMEBUFFER, state->framebuffer);
 			glReadPixels(0, 0, state->pre_render_width, state->pre_render_height, GL_RGB, GL_UNSIGNED_BYTE, buff);
 			glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-			printf("read pixel done!\n");
 
 			SaveJpeg(buff, state->pre_render_width, state->pre_render_height, state->snap_save_path, 70);
 
