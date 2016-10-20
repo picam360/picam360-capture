d630bc2200f605d0ff7f04fd33b00c15b4d12af2
takumadx <hirayama.takuma@ohmydigifab.com>
2016-10-20 20:50:57 +0900

    lunch

---
+++ lunch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+usage_exit() {
+        echo "Usage: $0 [-w width] [-h height] item ..." 1>&2
+        exit 1
+}
+
+CAM_WIDTH=1024
+CAM_HEIGHT=1024
+BITRATE=8000000
+RENDER_WIDTH=1440
+RENDER_HEIGHT=720
+
+while getopts w:h: OPT
+do
+    case $OPT in
+        w)  CAM_WIDTH=$OPTARG
+            ;;
+        h)  CAM_HEIGHT=$OPTARG
+            ;;
+        h)  usage_exit
+            ;;
+        \?) usage_exit
+            ;;
+    esac
+done
+
+raspivid -n -t 0 -w CAM_WIDTH -h CAM_HEIGHT -ih -b BITRATE -fps 5 -o - > cam0 &
+./picam360-oculus-viewer.bin -w CAM_WIDTH -h CAM_HEIGHT -W RENDER_WIDTH -H RENDER_HEIGHT -E -p < cmd &
