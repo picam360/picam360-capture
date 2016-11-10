e8760f5cc9eab49695009a9e43b926953c5de8f8
takumadx <hirayama.takuma@ohmydigifab.com>
2016-11-10 12:30:48 +0900

    tune

--- setup/set-oculus.sh
+++ setup/set-oculus.sh
@@ -7,5 +7,5 @@ if [ ! -e /boot/config.txt.set-oculus.bk ]; then
 	
 	sudo reboot
 else
-	sudo sh /home/pi/picam360/picam360-capture/lunch.sh -w 1440 -h 1440 -W 960 -H 960 -r -n 2 -s
+	sudo sh /home/pi/picam360/picam360-capture/lunch.sh -w 1440 -h 1440 -W 512 -H 512 -r -n 2 -s
 fi
