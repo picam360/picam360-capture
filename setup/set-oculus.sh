ddf76cb4bf1754bee88c4f646ad411132965042a
takumadx <hirayama.takuma@ohmydigifab.com>
2016-11-10 10:49:42 +0900

    rc.local

--- setup/set-oculus.sh
+++ setup/set-oculus.sh
@@ -1,5 +1,9 @@
 if [ ! -e /boot/config.txt.set-oculus.bk ]; then
 	sudo cp /boot/config.txt /boot/config.txt.set-oculus.bk
 	sudo cp /boot/config.txt.oculus /boot/config.txt
+	
+	sudo cp /etc/rc.local /etc/rc.local.set-oculus.bk
+	sudo cp /etc/rc.local.oculus /etc/rc.local
+	
 	sudo reboot
 fi
--- setup/unset-oculus.sh
+++ setup/unset-oculus.sh
@@ -1,5 +1,9 @@
 if [ -e /boot/config.txt.set-oculus.bk ]; then
 	sudo cp /boot/config.txt.set-oculus.bk /boot/config.txt
 	sudo rm /boot/config.txt.set-oculus.bk
+	
+	sudo cp /etc/rc.local.set-oculus.bk /etc/rc.local
+	sudo rm /etc/rc.local.set-oculus.bk
+	
 	sudo reboot
 fi
