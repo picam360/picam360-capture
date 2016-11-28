9d6363e21a88c537cbbe2bb3ac876b227f039a8e
takumadx <hirayama.takuma@ohmydigifab.com>
2016-11-28 22:51:25 +0900

    codec

--- lunch.sh
+++ lunch.sh
@@ -76,7 +76,7 @@ mkfifo cmd
 if [ $REMOTE = true ]; then
 	socat -u udp-recv:9000 - > cam0 & socat -u udp-recv:9001 - > cam1 &
 elif [ $DIRECT = "" ]; then
-	raspivid -n -t 0 -w $CAM_WIDTH -h $CAM_HEIGHT -ih -b $BITRATE -fps $FPS -o - > cam0 &
+	raspivid -cd $CODEC -n -t 0 -w $CAM_WIDTH -h $CAM_HEIGHT -ih -b $BITRATE -fps $FPS -o - > cam0 &
 fi
 
 if [ $BACKGROUND = true ]; then
