0499dc3445886e29e9be307b061f4a5fdf8111a2
takumadx <hirayama.takuma@ohmydigifab.com>
2016-11-29 12:37:06 +0900

    debug

--- video_mjpeg.c
+++ video_mjpeg.c
@@ -266,6 +266,8 @@ void *video_mjpeg_decode(void* arg) {
 			if (first_packet) {
 				buf->nFlags = OMX_BUFFERFLAG_STARTTIME;
 				first_packet = 0;
+			} else {
+				buf->nFlags = OMX_BUFFERFLAG_TIME_UNKNOWN;
 			}
 
 			if (eoi) {
