5ad51bc3add7a40c008adfe7606a6d931a9ddd7b
takumadx <hirayama.takuma@ohmydigifab.com>
2016-12-02 11:38:11 +0900

    image_receiver

--- video_mjpeg.c
+++ video_mjpeg.c
@@ -49,6 +49,79 @@ static void my_fill_buffer_done(void* data, COMPONENT_T* comp) {
 		exit(1);
 	}
 }
+#define MAX_IMAGE_QUEUE 4
+typedef struct _IMAGE_RECEIVER_DATA {
+	pthread_mutex_t mlock = PTHREAD_MUTEX_INITIALIZER;
+	int descriptor;
+	int image_queue_count;
+	int image_size_queue[MAX_IMAGE_QUEUE];
+	unsigned char *image_queue[MAX_IMAGE_QUEUE];
+} IMAGE_RECEIVER_DATA;
+
+void *image_receiver(void* arg) {
+	IMAGE_RECEIVER_DATA *data = (IMAGE_RECEIVER_DATA*) arg;
+	unsigned char buff[4096];
+	unsigned char image_buff = NULL;
+	int image_buff_size = 0;
+	int image_buff_cur = 0;
+	int image_start = -1;
+	int data_len = 0;
+	int data_len_total = 0;
+	int soicount = 0;
+	while (1) {
+		data_len = read(data->descriptor, buff, sizeof(buff));
+		for (int i = 0; i < data_len; i++) {
+			if (marker) {
+				marker = 0;
+				if (buff == 0xd8) { //SOI
+					if (soicount == 0) {
+						image_start = data_len_total + (i - 1);
+					}
+					soicount++;
+				}
+				if (buff == 0xd9 && image_start >= 0) { //EOI
+					soicount--;
+					if (soicount == 0) {
+						int image_size = (data_len_total + i + 1) - image_start;
+						if (image_buff == NULL) { //just allocate image buffer
+							image_buff_size = image_size * 2;
+						} else if (data->image_queue_count
+								< IMAGE_RECEIVER_DATA) {
+							memcpy(image_buff + image_buff_cur, buff, i);
+							pthread_mutex_lock(&data->mlock);
+							data->image_size_queue[data->image_queue_count] =
+									image_size;
+							data->image_queue[data->image_queue_count] =
+									image_buff;
+							data->image_queue_count++;
+							pthread_mutex_unlock(&data->mlock);
+							break;
+						} else { //throw it away
+							printf("image loss/n");
+							free(image_buff);
+						}
+						image_buff_cur = 0;
+						image_buff = malloc(image_buff_size);
+					}
+				}
+			} else if (buff == 0xff) {
+				marker = 1;
+			}
+		}
+		if (image_buff != NULL && image_start >= 0) {
+			if (image_buff_cur + data_len > image_buff_size) { //exceed buffer size
+				free(image_buff);
+				image_buff = NULL;
+				image_buff_size = 0;
+				image_buff_cur = 0;
+			} else {
+				memcpy(image_buff + image_buff_cur, buff, data_len);
+				image_buff_cur += data_len;
+			}
+		}
+		data_len_total += data_len;
+	}
+}
 
 // Modified function prototype to work with pthreads
 void *video_mjpeg_decode(void* arg) {
@@ -128,114 +201,124 @@ void *video_mjpeg_decode(void* arg) {
 		OMX_BUFFERHEADERTYPE *buf;
 		int port_settings_changed = 0;
 		int first_packet = 1;
-		int marker = 0;
-		int left_len = 0;
-		char *left_data = NULL;
 
 		ilclient_change_component_state(video_decode, OMX_StateExecuting);
 
 		printf("milestone\n");
 
-		while ((buf = ilclient_get_input_buffer(video_decode, 130, 1)) != NULL) {
+		IMAGE_RECEIVER_DATA data = { };
+		data.mlock = PTHREAD_MUTEX_INITIALIZER;
+		data.descriptor = descriptor;
+		pthread_t image_receiver_thread;
+		pthread_create(&image_receiver_thread, NULL, image_receiver,
+				(void*) &data);
 
-			int eoi = 0;
-			// feed data and wait until we get port settings changed
-			unsigned char *dest = buf->pBuffer;
-			if (left_len != 0) {
-				memcpy(dest, left_data, left_len);
-				free(left_data);
-				data_len = left_len;
-				left_len = 0;
-			} else {
-				data_len = read(descriptor, dest, buf->nAllocLen);
+		while (1) {
+			int image_cur = 0;
+			int image_size;
+			unsigned char *image_buff;
+
+			//wait untill image arived
+			if (data.image_queue_count <= 0) {
+				usleep(1000);
+				continue;
 			}
-			for (int i = 0; i < data_len; i++) {
-				if (marker) {
-					marker = 0;
-					if (buf->pBuffer[i] == 0xd9) { //EOI
-						eoi = 1;
-						left_len = data_len - (i + 1);
-						data_len = i + 1;
-						left_data = malloc(left_len);
-						memcpy(left_data, dest + data_len, left_len);
+
+			pthread_mutex_lock(&data.mlock);
+			data.image_queue_count--;
+			image_size = data.image_size_queue[data.image_queue_count];
+			image_buff = data.image_queue[data.image_queue_count];
+			if (data.image_queue_count > 0) {
+				memcpy(data.image_size_queue, data.image_size_queue + 1,
+						sizeof(int) * data.image_queue_count);
+				memcpy(data.image_buff, data.image_buff + 1,
+						sizeof(unsigned char*) * data.image_queue_count);
+			}
+			pthread_mutex_unlock(&data.mlock);
+
+			while (image_cur < image_size) {
+				buf = ilclient_get_input_buffer(video_decode, 130, 1);
+
+				data_len = min(buf->nAllocLen, image_size - image_cur);
+				memcpy(buf->pBuffer, image_buff + image_cur, data_len);
+				image_cur += data_len;
+
+				if (port_settings_changed == 0
+						&& ((data_len > 0
+								&& ilclient_remove_event(video_decode,
+										OMX_EventPortSettingsChanged, 131, 0, 0,
+										1) == 0)
+								|| (data_len == 0
+										&& ilclient_wait_for_event(video_decode,
+												OMX_EventPortSettingsChanged,
+												131, 0, 0, 1,
+												ILCLIENT_EVENT_ERROR
+														| ILCLIENT_PARAMETER_CHANGED,
+												10000) == 0))) {
+					port_settings_changed = 1;
+
+					if (ilclient_setup_tunnel(tunnel, 0, 0) != 0) {
+						status = -7;
 						break;
 					}
-				} else if (buf->pBuffer[i] == 0xff) {
-					marker = 1;
+
+					// Set egl_render to idle
+					ilclient_change_component_state(egl_render[index],
+							OMX_StateIdle);
+
+					// Enable the output port and tell egl_render to use the texture as a buffer
+					//ilclient_enable_port(egl_render, 221); THIS BLOCKS SO CAN'T BE USED
+					if (OMX_SendCommand(ILC_GET_HANDLE(egl_render[index]),
+							OMX_CommandPortEnable, 221, NULL)
+							!= OMX_ErrorNone) {
+						printf("OMX_CommandPortEnable failed.\n");
+						exit(1);
+					}
+
+					if (OMX_UseEGLImage(ILC_GET_HANDLE(egl_render[index]),
+							&eglBuffer[index], 221, NULL, eglImage[index])
+							!= OMX_ErrorNone) {
+						printf("OMX_UseEGLImage failed.\n");
+						exit(1);
+					}
+
+					// Set egl_render to executing
+					ilclient_change_component_state(egl_render[index],
+							OMX_StateExecuting);
+
+					// Request egl_render to write data to the texture buffer
+					if (OMX_FillThisBuffer(ILC_GET_HANDLE(egl_render[index]),
+							eglBuffer[index]) != OMX_ErrorNone) {
+						printf("OMX_FillThisBuffer failed.\n");
+						exit(1);
+					}
 				}
-			}
-
-			if (port_settings_changed == 0
-					&& ((data_len > 0
-							&& ilclient_remove_event(video_decode,
-									OMX_EventPortSettingsChanged, 131, 0, 0, 1)
-									== 0)
-							|| (data_len == 0
-									&& ilclient_wait_for_event(video_decode,
-											OMX_EventPortSettingsChanged, 131,
-											0, 0, 1,
-											ILCLIENT_EVENT_ERROR
-													| ILCLIENT_PARAMETER_CHANGED,
-											10000) == 0))) {
-				port_settings_changed = 1;
-
-				if (ilclient_setup_tunnel(tunnel, 0, 0) != 0) {
-					status = -7;
+				if (!data_len) {
 					break;
 				}
+				buf->nFilledLen = data_len;
 
-				// Set egl_render to idle
-				ilclient_change_component_state(egl_render[index],
-						OMX_StateIdle);
-
-				// Enable the output port and tell egl_render to use the texture as a buffer
-				//ilclient_enable_port(egl_render, 221); THIS BLOCKS SO CAN'T BE USED
-				if (OMX_SendCommand(ILC_GET_HANDLE(egl_render[index]),
-						OMX_CommandPortEnable, 221, NULL) != OMX_ErrorNone) {
-					printf("OMX_CommandPortEnable failed.\n");
-					exit(1);
+				buf->nOffset = 0;
+				if (first_packet) {
+					buf->nFlags = OMX_BUFFERFLAG_STARTTIME;
+					first_packet = 0;
+				} else {
+					buf->nFlags = OMX_BUFFERFLAG_TIME_UNKNOWN;
 				}
 
-				if (OMX_UseEGLImage(ILC_GET_HANDLE(egl_render[index]),
-						&eglBuffer[index], 221, NULL, eglImage[index])
+				if (image_cur >= image_size) {
+					buf->nFlags |= OMX_BUFFERFLAG_ENDOFFRAME;
+				}
+
+				if (OMX_EmptyThisBuffer(ILC_GET_HANDLE(video_decode), buf)
 						!= OMX_ErrorNone) {
-					printf("OMX_UseEGLImage failed.\n");
-					exit(1);
-				}
-
-				// Set egl_render to executing
-				ilclient_change_component_state(egl_render[index],
-						OMX_StateExecuting);
-
-				// Request egl_render to write data to the texture buffer
-				if (OMX_FillThisBuffer(ILC_GET_HANDLE(egl_render[index]),
-						eglBuffer[index]) != OMX_ErrorNone) {
-					printf("OMX_FillThisBuffer failed.\n");
-					exit(1);
+					status = -6;
+					break;
 				}
 			}
-			if (!data_len) {
-				break;
-			}
-			buf->nFilledLen = data_len;
 
-			buf->nOffset = 0;
-			if (first_packet) {
-				buf->nFlags = OMX_BUFFERFLAG_STARTTIME;
-				first_packet = 0;
-			} else {
-				buf->nFlags = OMX_BUFFERFLAG_TIME_UNKNOWN;
-			}
-
-			if (eoi) {
-				buf->nFlags |= OMX_BUFFERFLAG_ENDOFFRAME;
-			}
-
-			if (OMX_EmptyThisBuffer(ILC_GET_HANDLE(video_decode), buf)
-					!= OMX_ErrorNone) {
-				status = -6;
-				break;
-			}
+			//release memory
+			free(image_buff);
 		}
 
 		buf->nFilledLen = 0;
