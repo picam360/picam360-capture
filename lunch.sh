982492ada3b5e6943f691eb010ba3848115f43fd
takumadx <hirayama.takuma@ohmydigifab.com>
2017-01-04 15:08:35 +0900

    tune

--- lunch.sh
+++ lunch.sh
@@ -97,7 +97,8 @@ if [ $REMOTE = true ]; then
 	socat -u udp-recv:9000 - > cam0 & socat -u udp-recv:9001 - > cam1 &
 elif [ $DIRECT = ]; then
 	if [ $CODEC = "MJPEG" ]; then
-		raspivid -cd MJPEG -n -t 0 -w $CAM_WIDTH -h $CAM_HEIGHT -ex sports -b $BITRATE -fps $FPS -o - > cam0 &
+#		raspivid -cd MJPEG -n -t 0 -w $CAM_WIDTH -h $CAM_HEIGHT -ex sports -b $BITRATE -fps $FPS -o - > cam0 &
+		raspivid -cd MJPEG -n -t 0 -w $CAM_WIDTH -h $CAM_HEIGHT -b $BITRATE -fps $FPS -o - > cam0 &
 	else
 		raspivid -n -t 0 -w $CAM_WIDTH -h $CAM_HEIGHT -ex sports -ih -b $BITRATE -fps $FPS -o - > cam0 &
 	fi
