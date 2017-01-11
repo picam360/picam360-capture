4e756f35999b0f6a8074ba403345c3f78f316642
takumadx <hirayama.takuma@ohmydigifab.com>
2017-01-11 16:50:20 +0900

    ac multi cam

--- auto_calibration.cc
+++ auto_calibration.cc
@@ -22,6 +22,10 @@ extern "C" {
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
+typedef struct {
+	IplImage *imgAry[MAX_CAM_NUM];
+} AutoCalibrationData;
+
 static void auto_calibration(PICAM360CAPTURE_T *state, FRAME_T *frame);
 static void deinit(PICAM360CAPTURE_T *state, FRAME_T *frame);
 
@@ -41,19 +45,26 @@ static void auto_calibration(PICAM360CAPTURE_T *state, FRAME_T *frame) {
 	int offset_y = frame->height * state->options.cam_offset_y[0];
 
 	if (frame->custom_data == NULL) { // first call
-		frame->custom_data = (void*) cvCreateImage(cvSize(width, height),
-				IPL_DEPTH_8U, 1);
+		AutoCalibrationData *acd = malloc(sizeof(AutoCalibrationData));
+		memset(acd, 0, sizeof(AutoCalibrationData));
+		for (int i = 0; i < state->num_of_cam; i++) {
+			acd->imgAry[i] = vCreateImage(cvSize(width, height), IPL_DEPTH_8U,
+					1);
+			//reset
+			state->options.cam_offset_x[i] = 0.0;
+			state->options.cam_offset_y[i] = 0.0;
+		}
+		frame->custom_data = (void*) acd;
 
-		//reset
-		state->options.cam_offset_x[0] = 0.0;
-		state->options.cam_offset_y[0] = 0.0;
+		state->active_cam = 0;
 
 		return;
 	}
 
+	AutoCalibrationData *acd = (AutoCalibrationData*) frame->custom_data;
 	CvMemStorage* storage = cvCreateMemStorage(0);
 	CvSeq* contour = NULL;
-	IplImage *img = (IplImage*) frame->custom_data;
+	IplImage *img = (IplImage*) acd->imgAry[state->active_cam];
 	IplImage *image_bin = cvCreateImage(cvSize(width, height), IPL_DEPTH_8U, 1);
 
 	//binalize
@@ -64,9 +75,12 @@ static void auto_calibration(PICAM360CAPTURE_T *state, FRAME_T *frame) {
 			uint32_t _x = x - margin - offset_x;
 			uint32_t _y = y - margin - offset_y;
 			if (_x >= 0 && _x < frame->width && _y >= 0 && _y < frame->height) {
-				uint8_t ch1 = (frame->img_buff + frame->width * 3 * _y)[_x * 3 + 0];
-				uint8_t ch2 = (frame->img_buff + frame->width * 3 * _y)[_x * 3 + 1];
-				uint8_t ch3 = (frame->img_buff + frame->width * 3 * _y)[_x * 3 + 2];
+				uint8_t ch1 = (frame->img_buff + frame->width * 3 * _y)[_x * 3
+						+ 0];
+				uint8_t ch2 = (frame->img_buff + frame->width * 3 * _y)[_x * 3
+						+ 1];
+				uint8_t ch3 = (frame->img_buff + frame->width * 3 * _y)[_x * 3
+						+ 2];
 				val = MAX(ch1, MAX(ch2, ch3));
 			}
 
@@ -106,8 +120,10 @@ static void auto_calibration(PICAM360CAPTURE_T *state, FRAME_T *frame) {
 		//printf("%lf,%lf\n", box.center.x, box.center.y);
 		if (box.size.width > (float) frame->width * 0.8
 				&& box.size.height > (float) frame->height * 0.8) {
-			state->options.cam_offset_x[0] = box.center.x / width - 0.5;
-			state->options.cam_offset_y[0] = box.center.y / height - 0.5;
+			state->options.cam_offset_x[state->active_cam] = box.center.x
+					/ width - 0.5;
+			state->options.cam_offset_y[state->active_cam] = box.center.y
+					/ height - 0.5;
 		}
 	}
 
@@ -119,12 +135,20 @@ static void auto_calibration(PICAM360CAPTURE_T *state, FRAME_T *frame) {
 		cvReleaseImage(&image_bin);
 	}
 
+	state->active_cam++;
+	if (state->active_cam >= state->num_of_cam) {
+		state->active_cam = 0;
+	}
 }
 
 static void deinit(PICAM360CAPTURE_T *state, FRAME_T *frame) {
 	if (frame->custom_data == NULL) {
-		IplImage *img = (IplImage*) frame->custom_data;
-		cvReleaseImage(&img);
-		frame->custom_data = NULL;
+		return;
 	}
+	AutoCalibrationData *acd = (AutoCalibrationData*) frame->custom_data;
+	for (int i = 0; i < state->num_of_cam; i++) {
+		cvReleaseImage(&acd->imgAry[i]);
+	}
+	free(acd);
+	frame->custom_data = NULL;
 }
