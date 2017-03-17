31cbcc42a2a76028aa91b5958eedbbdd9d685913
takumadx <hirayama.takuma@ohmydigifab.com>
2017-03-17 17:38:40 +0900

    debug

--- plugins/driver_agent/driver_agent.c
+++ plugins/driver_agent/driver_agent.c
@@ -111,13 +111,13 @@ void *transmit_thread_func(void* arg) {
 		gettimeofday(&time, NULL);
 		struct timeval diff;
 		timersub(&time, &last_time, &diff);
-		int diff_usec = diff.tv_sec * 1000 + diff.tv_usec;
+		int diff_usec = diff.tv_sec * 1000000 + diff.tv_usec;
 		//cal
 		if (!lowlevel_control) {
 			lg_light_value[0] = lg_light_strength;
 			lg_light_value[1] = lg_light_strength;
 
-			lg_thrust *= 1.0 - (lg_brake_ps / 100) * (diff_usec / 1000);
+			lg_thrust *= 1.0 - (lg_brake_ps / 100) * (diff_usec / 1000000);
 			lg_motor_value[0] = lg_thrust;
 			lg_motor_value[1] = lg_thrust;
 			lg_motor_value[2] = lg_thrust;
@@ -126,8 +126,8 @@ void *transmit_thread_func(void* arg) {
 		//kokuyoseki func
 		if (lg_last_button == BLACKOUT_BUTTON && lg_func != -1) {
 			timersub(&time, &lg_last_kokuyoseki_time, &diff);
-			diff_usec = diff.tv_sec * 1000 + diff.tv_usec;
-			if (diff_usec > 1000) {
+			diff_usec = diff.tv_sec + diff.tv_usec;
+			if (diff_usec > 1000000) {
 				switch (lg_func) {
 				case 1:
 					lg_light_strength = 0;
@@ -195,7 +195,7 @@ static void kokuyoseki_callback(struct timeval time, int button, int value) {
 	}
 	struct timeval diff;
 	timersub(&time, &lg_last_kokuyoseki_time, &diff);
-	int diff_usec = diff.tv_sec * 1000 + diff.tv_usec;
+	int diff_usec = diff.tv_sec * 1000000 + diff.tv_usec;
 	switch (button) {
 	case NEXT_BUTTON:
 		lg_thrust += 1;
@@ -204,12 +204,12 @@ static void kokuyoseki_callback(struct timeval time, int button, int value) {
 		lg_thrust -= 1;
 		break;
 	case NEXT_BUTTON_LONG:
-		if (diff_usec < 500)
+		if (diff_usec < 500000)
 			return;
 		lg_light_strength += 1;
 		break;
 	case BACK_BUTTON_LONG:
-		if (diff_usec < 500)
+		if (diff_usec < 500000)
 			return;
 		lg_light_strength -= 1;
 		break;
