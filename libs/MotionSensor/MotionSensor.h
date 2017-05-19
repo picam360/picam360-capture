2de4ad35514a439e26a54ee587ddcc32355549ff
takumadx <hirayama.takuma@ohmydigifab.com>
2017-05-19 12:02:38 +0900

    debug

--- libs/MotionSensor/MotionSensor.h
+++ libs/MotionSensor/MotionSensor.h
@@ -2,8 +2,8 @@
 #define _MOTION_SENSOR_H_
 
 #define YAW 0
-#define PITCH 1
-#define ROLL 2
+#define ROLL 1
+#define PITCH 2
 #define DIM 3
 #ifdef __cplusplus
 extern "C"{
@@ -16,7 +16,7 @@ extern float gyro[3];
 extern float temp;
 extern float compass[3];
 
-extern int ms_open();
+extern int ms_open(int i2c_ch);
 extern int ms_update();
 extern int ms_close();
 
