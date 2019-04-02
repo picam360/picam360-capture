90faa3577c170098cf10c15c728245c31aed17e0
takumadx <hirayama.takuma@ohmydigifab.com>
2019-04-02 19:39:46 +0900

    clean

--- setup/clean-up.sh
+++ setup/clean-up.sh
@@ -5,16 +5,25 @@ cmake .
 make
 sudo make install
 
-cd /home/pi/picam360/picam360-server
+cd /home/pi/picam360/armv6l/picam360-server
 sudo rm -r userdata
 mkdir userdata
 git remote set-url origin https://github.com/picam360/picam360-server.git
 git reset --hard & git pull
-cmake .
-make
-sudo make install
+echo "make sure armv6l/picam360-server ready"
 
-cd /home/pi/picam360/picam360-server/www
+cd /home/pi/picam360/armv7l/picam360-server
+sudo rm -r userdata
+mkdir userdata
+git remote set-url origin https://github.com/picam360/picam360-server.git
+git reset --hard & git pull
+echo "make sure armv7l/picam360-server ready"
+
+cd /home/pi/picam360/armv6l/picam360-server/www
+git remote set-url origin https://github.com/picam360/picam360-viewer.git
+git reset --hard & git pull
+
+cd /home/pi/picam360/armv7l/picam360-server/www
 git remote set-url origin https://github.com/picam360/picam360-viewer.git
 git reset --hard & git pull
 
