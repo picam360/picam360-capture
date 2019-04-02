ac84ab606bf4637bc8f17c7f92418eeaa138e6f0
takumadx <hirayama.takuma@ohmydigifab.com>
2019-04-02 18:06:09 +0900

    conf file not to be overwritten

--- setup/setup-dhcp-client.sh
+++ setup/setup-dhcp-client.sh
@@ -3,6 +3,7 @@ CURRENT=$(cd $(dirname $0) && pwd)
 cd $CURRENT
 
 sudo rm /etc/network/interfaces.d/wlan0-dhcp-server
+sudo cp wlan0-dhcp-client /etc/network/interfaces.d/
 sudo systemctl disable hostapd
 sudo systemctl disable isc-dhcp-server
 sudo sed -e 's/^denyinterfaces wlan0/#denyinterfaces wlan0/g' /etc/dhcpcd.conf > dhcpcd.conf.tmp
--- setup/setup-dhcp-server.sh
+++ setup/setup-dhcp-server.sh
@@ -2,6 +2,7 @@
 CURRENT=$(cd $(dirname $0) && pwd)
 cd $CURRENT
 
+sudo rm /etc/network/interfaces.d/wlan0-dhcp-client
 sudo cp wlan0-dhcp-server /etc/network/interfaces.d/
 sudo systemctl enable hostapd
 sudo systemctl enable isc-dhcp-server
