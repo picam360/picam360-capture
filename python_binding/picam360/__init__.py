de36b6c0858b3119a6f213c8529dc3f18e62a8df
takumadx <hirayama.takuma@ohmydigifab.com>
2019-07-19 10:28:43 +0900

    python_binding

---
+++ python_binding/picam360/__init__.py
@@ -0,0 +1,1 @@
+from .picam360 import Picam360
---
+++ python_binding/setup.py
@@ -0,0 +1,14 @@
+import glob
+import subprocess
+from setuptools import setup, find_packages, Extension
+
+
+setup(
+    name='picam360',
+    version='0.0.1',
+    description='An open-source panorama camera',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    package_data={},
+)
